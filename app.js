d267c349892126741cdaa5cabef2114a6635c11d
Neil M <n.mckenzie@live.com>
2020-10-22 15:49:12 -0500

    set up query paths

--- app.js
+++ app.js
@@ -5,6 +5,43 @@ const PORT = 8080;
 
 const app = express();
 
+app.use(express.static('public'));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
+app.get('/api/notes', (req, res) => {
+  res.json(noteJSON);
+});
+
+
+app.post('/api/notes', (req, res) => {
+const lastId = parseInt(noteJSON.length);
+const id = lastId + 1;
+noteJSON.push( { id, ...req.body} );
+res.json(noteJSON);
+});
+
+
+app.delete('/api/notes/:id', (req, res) => {
+const { id } = req.params.id
+const noteIndex = noteJSON.findIndex(n => n.id === id);
+noteJSON.splice(noteIndex, 1);
+res.end("DELETION");
+});
 
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
